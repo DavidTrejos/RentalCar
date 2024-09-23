693dd371e390c8cba52809107e69a428f5f0476e
David Trejos <davidtrejoscortes@gmail.com>
2024-09-22 21:42:23 -0500

    initial phase web application: List the cars and show in console the register.

--- src/app/rental-form/rental-form.component.ts
+++ src/app/rental-form/rental-form.component.ts
@@ -1,19 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { CarService } from '../car-service.service';
 
 @Component({
   standalone: true,
   selector: 'app-rental-form',
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, HttpClientModule,ReactiveFormsModule],
   templateUrl: './rental-form.component.html',
   styleUrls: ['./rental-form.component.css']
 })
 export class RentalFormComponent implements OnInit {
   cars: any[] = [];
+  rentalForm: FormGroup;
 
-  constructor(private carService: CarService) { }
+  constructor(private carService: CarService) {
+
+    this.rentalForm = new FormGroup({
+      car: new FormControl(''),
+      rentalDate: new FormControl('')
+    });
+
+
+  }
 
   ngOnInit(): void {
     this.carService.getCars().subscribe({
@@ -28,5 +38,9 @@ export class RentalFormComponent implements OnInit {
         console.log('Car fetching completed');
       }
     });
+  }
+
+  onSubmit(): void {
+    console.log(this.rentalForm.value);
   }
 }
